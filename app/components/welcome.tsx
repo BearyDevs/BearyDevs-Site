5b4ca32ed4c37c4f1af4d007d8ef9377e473af64
BEARYDEV <teerapat.tech@gmail.com>
2024-08-25 20:52:59 +0700

    fix build error

--- app/components/welcome.tsx
+++ app/components/welcome.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Image, Link } from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
 import classNames from "classnames";
 import { useState, useEffect } from "react";
 
