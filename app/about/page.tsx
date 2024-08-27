b53d00eca113b03eef42c51c42641bcd9d4b34e7
BEARYDEV <teerapat.tech@gmail.com>
2024-08-27 13:58:53 +0700

    update about/page more

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,8 +12,8 @@ export default function AboutPage() {
         {`Let's get to know me!`.toUpperCase()}
       </h1>
       <p>{`I'm a developer from Bangkok, Thailand, with ${yearsOfExperience}+ years of experience in web development.`}</p>
-      <p>{`I've poured all my skills and creativity into my work and have a passion for programming. I'm always on the lookout for new challenges.`}</p>
-      <p>{`I'm eager to grow and continuously learn new things.`}</p>
+      <p></p>
+      <p>{`I've poured all my skills and creativity into my work and have a passion for programming. I'm always on the lookout for new challenges and eager to grow and continuously learn new things.`}</p>
     </div>
   );
 }
