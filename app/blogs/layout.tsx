d31bff076a10730ba68de584745da011cc107f53
BEARYDEV <teerapat.tech@gmail.com>
2024-08-27 22:38:37 +0700

    update blogs

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 
 export default function MyBlogsLayout({ children }: PropsWithChildren) {
   return (
-    <div className="w-full h-full flex items-start justify-center overflow-hidden">
-      <div className="animate-[fadeIn_1s] flex-grow w-[85%] max-w-screen h-auto overflow-auto py-1 px-4 pl-6 custom900:pl-0">
+    <div className="w-full h-full flex items-start justify-center">
+      <div className="animate-[fadeIn_1s] w-[100dvw] sm:w-[85dvw] h-auto max-h-max py-1 lg:pb-6 px-2 sm:px-4 pl-6 custom900:pl-0">
         {children}
       </div>
     </div>
