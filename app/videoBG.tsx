e5732cbcdd0a9260cf09fc17ded0239bcdc4a6e9
BEARYDEV <teerapat.tech@gmail.com>
2024-08-28 13:41:37 +0700

    new video bg

--- app/videoBG.tsx
+++ app/videoBG.tsx
@@ -2,38 +2,17 @@
 
 import dynamic from "next/dynamic";
 import React, { useCallback } from "react";
-import { useSelector } from "react-redux";
-import { ReduxState } from "./libs/redux/store";
 
 const VideoRender = dynamic(() => import("./videoRender"), {
   ssr: false,
 });
 
 function VideoBG() {
-  const minimized = useSelector((state: ReduxState) => state.minimized);
-
   const Video = useCallback(() => {
     return <VideoRender />;
   }, []);
 
-  return minimized ? (
-    <Video />
-  ) : (
-    <div
-      style={{
-        backgroundImage: "url(/bg.webp)",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        height: "100%",
-        width: "100%",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        opacity: 0.15,
-      }}
-    />
-  );
+  return <Video />;
 }
 
 export default React.memo(VideoBG);
--- app/videoRender.tsx
+++ app/videoRender.tsx
@@ -1,11 +1,21 @@
-import classNames from "classnames";
+"use client";
 
-export default function VideoRender() {
+import React from "react";
+import classNames from "classnames";
+import { useSelector } from "react-redux";
+import { ReduxState } from "./libs/redux/store";
+
+function VideoRender() {
+  const minimized = useSelector((state: ReduxState) => state.minimized);
+
   return (
     <video
       className={classNames({
-        "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-screen min-h-screen object-cover transition-all duration-1000 opacity-50":
+        "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-screen min-h-screen object-cover transition-all duration-500":
           true,
+        "hidden lg:block": true,
+        "opacity-15": !minimized,
+        "opacity-50": minimized,
       })}
       autoPlay={true}
       muted
@@ -15,3 +25,5 @@ export default function VideoRender() {
     </video>
   );
 }
+
+export default React.memo(VideoRender);
