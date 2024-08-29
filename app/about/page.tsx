f0cff169e8e19ca61d3a7d6529ef7aeb18aacfcb
BEARYDEV <teerapat.tech@gmail.com>
2024-08-29 09:34:27 +0700

    Change year experience

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
 export default function AboutPage() {
-  const startYear = new Date("2021-03-01");
+  const startYear = new Date("2020-03-01");
   const currentYear = new Date();
   const yearsOfExperience =
     currentYear.getFullYear() -
@@ -11,7 +11,7 @@ export default function AboutPage() {
       <h1 className="font-bold mb-2">
         {`Let's get to know me!`.toUpperCase()}
       </h1>
-      <p>{`I'm a developer from Bangkok, Thailand, with ${yearsOfExperience}+ years of experience in web development.`}</p>
+      <p>{`I'm a developer from Bangkok, Thailand, with ${yearsOfExperience}+ years of experience.`}</p>
       <p></p>
       <p>{`I've poured all my skills and creativity into my work and have a passion for programming and always on the lookout for new challenges and eager to grow and continuously learn new things.`}</p>
     </div>
