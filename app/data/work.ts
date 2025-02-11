86aab2065567b6f7c2ca4cd70532ed0b4284867d
BearyDevs <73677545+BearyDevs@users.noreply.github.com>
2025-02-11 23:02:51 +0700

    Update work.ts

--- app/data/work.ts
+++ app/data/work.ts
@@ -34,7 +34,7 @@ let ORM = ["Prisma", "MongoDB"];
 
 let databases = ["MySQL", "PostgreSQL", "MongoDB", "Redis"];
 
-let containers = ["Docker"];
+let containers = ["Docker"]
 
 let tools = [
   "Git",
@@ -43,6 +43,10 @@ let tools = [
   "BitBucket",
   "MS Teams",
   "Confluence",
+  "Jenkins",
+  "Jest",
+  "Vitest",
+  "Playwright",
   "Postman",
   "Figma",
   "Vercel",
